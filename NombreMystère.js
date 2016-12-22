2620e2f317d39d355bdf098a905c7ec12b8d9807
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2016-12-22 18:27:08 +0100

    Fini bonus
    David Gena Quentin Cyril

--- NombreMystère.js
+++ NombreMystère.js
@@ -1,3 +1,28 @@
+<<<<<<< HEAD
+var min = prompt("min");
+var max = prompt("max"); 
+
+
+var numRand = Math.floor(Math.random() * ( Number(max) - Number(min) ) + Number(min) );
+
+var nbTentative = prompt("combien d'essais ?");
+
+var numUser = prompt("entrer un chiffre positif");
+for(i = 0; i < nbTentative-1; i++){
+
+  if(numRand > numUser){
+    var numUser = prompt("c'est plus")
+  }
+  
+  else if(numRand < numUser){
+    var numUser = prompt("c'est moins");
+  }
+  
+  else if(numRand == numUser){
+    alert("c'est gagné !!");
+    break;
+  }
+=======
 var min = prompt("min");
 var max = prompt("max");
 
@@ -20,4 +45,5 @@ for(i = 0; i < nbTentative-1; i++){
     alert("c'est gagné !!");
     break;
   }
+>>>>>>> origin/master
 }
